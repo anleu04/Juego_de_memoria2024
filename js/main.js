6886c9942b7897fc21d3bf72e93be9e8af6ba52e
Live session user <josueanleu4@gmail.com>
2024-02-01 19:27:08 +0000

    Repartir Cartas

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let cards1= ["🥸","👽","🥱","😎","🫢","😏","🙂","🙃","😮‍💨","😡","🤯","🫣"]
+
+let cards2= ["🥸","👽","🥱","😎","🫢","😏","🙂","🙃","😮‍💨","😡","🤯","🫣"]
+
+let todas_las_tarjetas = cards1.concat(cards2);
+
+function repartir_cartas(){
+   let tablero = document.querySelector("#root");
+   
+   todas_las_tarjetas.forEach((cada_carta)=> {
+      let cart = document.createElement("div")
+   cart.innerHTML = `<div class= "carta">${cada_carta}</div>`
+
+   tablero.appendChild(cart);})
+   
+
+   
+
+}
+
+repartir_cartas();
